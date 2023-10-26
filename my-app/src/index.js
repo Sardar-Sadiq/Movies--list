684b71d7349ef7fbcb4ef937646168ea5b53356e
sardarsadiq <119177243+Sardar-Sadiq@users.noreply.github.com>
2023-10-26 10:29:05 +0530

    Delete my-app/src directory

--- my-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom'; //Dom aako capital mai hana naitho error aatha 
-
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
-
-// ReactDOM.render(<App />, document.getElementById('root'));
